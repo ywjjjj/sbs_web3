b1dfc0e479352df60e1f8949ac13377d6c31586a
ywjjjj <yeniyeni12@naver.com>
2022-04-19 12:48:53 +0900

    Day07

--- Day06/js/04_animate.js
+++ Day06/js/04_animate.js
@@ -31,4 +31,118 @@ $(function() {
     *인자2: function(함수)
             - 애니메이션이 완료되면 호출할 콜백함수
 */
+
+    // item2
+    /* 
+        fadeXXX(인자1, 인자2)
+        * 인자1: duration (시간: ms), ('slow', 'fast')
+        * 인자2: function (애니메이션 후, 콜백함수)
+        
+        fadeXXX(인자1, 인자2, 인자3)
+        * 인자1: duration (시간: ms), ('slow', 'fast')
+        * 인자2: easing (swing[기본값], linear)
+        * 인자3: function (애니메이션 후, 콜백함수)
+        
+        fadeTo(인자1, 인자2, 인자3)
+        * 인자1: duration (시간: ms), ('slow', 'fast')
+        * 인자2: opacity (불투명도: 0.0 ~ 1.0)
+        * 인자3: function (애니메이션 후, 콜백함수)
+        
+        fadeTo(인자1, 인자2, 인자3)
+        * 인자1: duration (시간: ms), ('slow', 'fast')
+        * 인자2: opacity (불투명도: 0.0 ~ 1.0)
+        * 인자3: easing (swing[기본값], linear)
+        * 인자4: function (애니메이션 후, 콜백함수)
+        
+    */
+    // fadeOut 버튼 클릭 이벤트
+    $('#item2 .fadeOut').on('click', function() {
+        $('#item2 .target-box').fadeOut(1000)
+    })
+
+    // fadeIn 버튼 클릭 이벤트
+    $('#item2 .fadeIn').on('click', function() {
+        $('#item2 .target-box').fadeIn(1000)
+    })
+
+    // fadeToggle 버튼 클릭 이벤트
+    $('#item2 .fadeToggle').on('click', function() {
+        $('#item2 .target-box').stop().fadeToggle(1000)
+    })
+
+    // fadeTo 버튼 클릭 이벤트
+    $('#item2 .fadeTo').on('click', function() {
+        $('#item2 .target-box').fadeTo('slow', 0.2)
+    })
+
+
+    // item3
+    /*
+        slideXXX(인자1, 인자2)
+        * 인자1: duration (시간: ms), ('slow', 'fast')
+        * 인자2: function (애니메이션 후, 콜백함수)
+        
+        slideXXX(인자1, 인자2, 인자3)
+        * 인자1: duration (시간: ms), ('slow', 'fast')
+        * 인자2: easing (swing[기본값], linear)
+        * 인자3: function (애니메이션 후, 콜백함수)
+        
+    */
+    // slideUp 버튼 클릭 이벤트
+    $('#item3 .slideUp').on('click',function() {
+        $('#item3 .target-box').slideUp(1000)
+    })
+
+    // slideDown 버튼 클릭 이벤트
+    $('#item3 .slideDown').on('click',function() {
+        $('#item3 .target-box').slideDown(1000)
+    })
+
+    // slideToggle 버튼 클릭 이벤트
+    $('#item3 .slideToggle').on('click',function() {
+        $('#item3 .target-box').stop().slideToggle(1500,'linear', function() {
+            alert('슬라이드 끝!')
+        })
+    })
+
+
+    /* item4 */
+    let item4Clicked = 'off'        // animate 버튼 클릭 여부
+    $('#item4 .animate').on('click', function() {
+
+        if(item4Clicked == 'off') {
+            // animate ('스타일', 시간)
+            $('#item4 .target-box').stop().animate({
+                'opacity': '0.3',
+                'width': '400px'
+            }, 3000)
+            item4Clicked = 'on'
+        } else {
+            $('#item4 .target-box').stop().animate({
+                'opacity': '1',
+                'width': '150px'
+            }, 3000)
+            item4Clicked='off'
+        }
+
+    })
+
+
+    // item5
+    /*
+        .animate() 메서드에서는 적용할 수 있는 스타일 속성이 제한되어 있다.
+        * 원하는 스타일을 지정하여 애니메이션을 적용하는 방법
+        1. 이벤트 정의
+            ex. click 이벤트
+        2. 애니메이션 적용될 요소에 'active'라는 클래스 추가/삭제
+            - toggleClass('active')
+        3. CSS에서 해당 요소에 .active 종속선택자를 지정하여
+            animation 속성을 지정
+    */
+   $('#item5 .animate').on('click', function() {
+       $('#item5 .target-box').toggleClass('active')
+       
+   })
+
+
 })
